e3abde3c900758bfd72a6eedf2800d596013e75f
Peer Xu <pppeerxu@gmail.com>
2018-05-29 11:30:27 +0800

    :tada: add dispatcher for motor

---
+++ pkg/motor/plugin/dispatcher/list.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/any"
+
+	pb "github.com/nayotta/metathings/pkg/proto/motor"
+)
+
+func unary_list(cli pb.MotorServiceClient, ctx context.Context, req *any.Any) (*any.Any, error) {
+	req1 := new(pb.ListRequest)
+	err := ptypes.UnmarshalAny(req, req1)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := cli.List(ctx, req1)
+	if err != nil {
+		return nil, err
+	}
+
+	res1, err := ptypes.MarshalAny(res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res1, nil
+}
+
+func init() {
+	unary_call_methods["List"] = unary_list
+}
--- pkg/motor/plugin/dispatcher/plugin.go
+++ pkg/motor/plugin/dispatcher/plugin.go
@@ -5,22 +5,63 @@ import (
 
 	"github.com/golang/protobuf/ptypes/any"
 
+	client_helper "github.com/nayotta/metathings/pkg/common/client"
 	opt_helper "github.com/nayotta/metathings/pkg/common/option"
 	mt_plugin "github.com/nayotta/metathings/pkg/core/plugin"
+	pb "github.com/nayotta/metathings/pkg/proto/motor"
 )
 
-type motorDispatcherPlugin struct{}
+type motorDispatcherPlugin struct {
+	cli_fty *client_helper.ClientFactory
+}
 
 func (dp *motorDispatcherPlugin) Init(opt opt_helper.Option) error {
+	cfgs := client_helper.NewDefaultServiceConfigs(opt.GetString("endpoint"))
+	cli_fty, err := client_helper.NewClientFactory(cfgs, client_helper.WithInsecureOptionFunc())
+	if err != nil {
+		return err
+	}
+
+	dp.cli_fty = cli_fty
+
 	return nil
 }
 
+var (
+	unary_call_methods = map[string]func(pb.MotorServiceClient, context.Context, *any.Any) (*any.Any, error){}
+)
+
 func (dp *motorDispatcherPlugin) UnaryCall(method string, ctx context.Context, req *any.Any) (*any.Any, error) {
-	return nil, nil
+	cli, cfn, err := dp.cli_fty.NewMotorServiceClient()
+	if err != nil {
+		return nil, err
+	}
+	defer cfn()
+
+	fn, ok := unary_call_methods[method]
+	if !ok {
+		return nil, mt_plugin.ErrUnknownMethod
+	}
+
+	return fn(cli, ctx, req)
 }
 
+var (
+	stream_call_methods = map[string]func(cli pb.MotorServiceClient, ctx context.Context, cbs ...func()) (mt_plugin.Stream, error){}
+)
+
 func (dp *motorDispatcherPlugin) StreamCall(method string, ctx context.Context) (mt_plugin.Stream, error) {
-	return nil, nil
+	cli, cfn, err := dp.cli_fty.NewMotorServiceClient()
+	if err != nil {
+		return nil, err
+	}
+
+	fn, ok := stream_call_methods[method]
+	if !ok {
+		return nil, mt_plugin.ErrUnknownMethod
+	}
+
+	return fn(cli, ctx, cfn)
 }
 
 func NewDispatcherPlugin() mt_plugin.DispatcherPlugin {
