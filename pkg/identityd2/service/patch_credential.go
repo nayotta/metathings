075b46ac5f38cb26713db6073d0610481af7e5b6
zhanghuichina <zhanghui0000@gmail.com>
2018-10-30 13:34:41 +0800

    typo correct

--- pkg/identityd2/service/patch_credential.go
+++ pkg/identityd2/service/patch_credential.go
@@ -35,7 +35,7 @@ func (self *MetathingsIdentitydService) PatchCredential(ctx context.Context, req
 	}
 
 	if cred, err = self.storage.PatchCredential(id_str, cred); err != nil {
-		self.logger.WithError(err).Errorf("failed to get credential in storage")
+		self.logger.WithError(err).Errorf("failed to patch credential in storage")
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 
