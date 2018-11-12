2f434d3c61c7a568c9c5cdd5789e49afa89a3518
Peer Xu <pppeerxu@gmail.com>
2018-11-12 17:30:17 +0800

    :art: format code

--- pkg/identityd2/service/patch_credential.go
+++ pkg/identityd2/service/patch_credential.go
@@ -3,11 +3,12 @@ package metathings_identityd2_service
 import (
 	"context"
 	"errors"
+	
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	storage "github.com/nayotta/metathings/pkg/identityd2/storage"
 	pb "github.com/nayotta/metathings/pkg/proto/identityd2"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (self *MetathingsIdentitydService) PatchCredential(ctx context.Context, req *pb.PatchCredentialRequest) (*pb.PatchCredentialResponse, error) {
