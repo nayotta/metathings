a443c4b22e03588a36ccdd7b5dd3dc650b0c0295
zhanghuichina <zhanghui0000@gmail.com>
2018-11-13 17:16:52 +0800

    :art: merge error fix

--- pkg/proto/identityd2/list_entities.validator.pb.go
+++ pkg/proto/identityd2/list_entities.validator.pb.go
@@ -5,6 +5,7 @@ package identityd2
 
 import github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 import proto "github.com/golang/protobuf/proto"
+import fmt "fmt"
 import math "math"
 import _ "github.com/golang/protobuf/ptypes/wrappers"
 import _ "github.com/mwitkow/go-proto-validators"
