e4dffd56e2ce8fa4e4425ebff4d79818d0c81e4a
Peer Xu <pppeerxu@gmail.com>
2018-11-14 14:07:10 +0800

    :construction: delete device  with validator and authorizion for service

--- pkg/deviced/service/delete_device.go
+++ pkg/deviced/service/delete_device.go
@@ -7,9 +7,26 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	policy_helper "github.com/nayotta/metathings/pkg/common/policy"
 	storage "github.com/nayotta/metathings/pkg/deviced/storage"
 	pb "github.com/nayotta/metathings/pkg/proto/deviced"
 )
+
+func (self *MetathingsDevicedService) ValidateDeleteDevice(ctx context.Context, in interface{}) error {
+	return self.validate_chain(
+		[]interface{}{
+			func() (policy_helper.Validator, get_devicer) {
+				req := in.(*pb.DeleteDeviceRequest)
+				return req, req
+			},
+		},
+		[]interface{}{ensure_get_device_id},
+	)
+}
+
+func (self *MetathingsDevicedService) AuthorizeDeleteDevice(ctx context.Context, in interface{}) error {
+	return self.enforce(ctx, in.(*pb.DeleteDeviceRequest).GetDevice().GetId().GetValue(), "delete_device")
+}
 
 func (self *MetathingsDevicedService) DeleteDevice(ctx context.Context, req *pb.DeleteDeviceRequest) (*empty.Empty, error) {
 	var dev *storage.Device
@@ -22,11 +39,18 @@ func (self *MetathingsDevicedService) DeleteDevice(ctx context.Context, req *pb.
 	}
 
 	for _, m := range dev.Modules {
-		if err = self.storage.DeleteModule(*m.Id); err != nil {
-			self.logger.WithError(err).WithField("id", *m.Id).Warningf("failed to delete module in storage")
+		mdl_id_str := *m.Id
+		if err = self.enforcer.RemoveObjectFromKind(mdl_id_str, KIND_MODULE); err != nil {
+			self.logger.WithError(err).Warningf("failed to remove module from kind in enforcer")
+		}
+		if err = self.storage.DeleteModule(mdl_id_str); err != nil {
+			self.logger.WithError(err).WithField("id", mdl_id_str).Warningf("failed to delete module in storage")
 		}
 	}
 
+	if err = self.enforcer.RemoveObjectFromKind(dev_id_str, KIND_DEVICE); err != nil {
+		self.logger.WithError(err).Warningf("failed to remove device from kind in enforcer")
+	}
 	if err = self.storage.DeleteDevice(dev_id_str); err != nil {
 		self.logger.WithError(err).Debugf("failed to delete device in storage")
 		return nil, status.Errorf(codes.Internal, err.Error())
