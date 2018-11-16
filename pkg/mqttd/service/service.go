25f33813c0c57bba1d8f2c2f3462764f280ab322
zhanghuichina <zhanghui0000@gmail.com>
2018-11-16 16:31:20 +0800

    :tada: add unarycall for service

--- pkg/mqttd/service/service.go
+++ pkg/mqttd/service/service.go
@@ -134,10 +134,6 @@ func (sev *MetathingsMqttdService) AuthFuncOverride(ctx context.Context, fullMet
 	return newCtx, nil
 }
 
-func (sev *MetathingsMqttdService) UnaryCall(context.Context, *pb.UnaryCallRequest) (*pb.UnaryCallResponse, error) {
-
-}
-
 func (sev *MetathingsMqttdService) StreamCall(context.Context, *pb.StreamCallRequest) (*pb.StreamCallResponse, error) {
 
 }
