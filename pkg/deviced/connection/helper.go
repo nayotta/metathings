7204b4ab7248d3a51d8e781bbd9456ba76f85ae4
Peer Xu <pppeerxu@gmail.com>
2019-01-10 15:19:18 +0800

    :tada: handle stream message

--- pkg/deviced/connection/helper.go
+++ pkg/deviced/connection/helper.go
@@ -3,8 +3,12 @@ package metathings_deviced_connection
 import (
 	"fmt"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/wrappers"
+
 	id_helper "github.com/nayotta/metathings/pkg/common/id"
 	rand_helper "github.com/nayotta/metathings/pkg/common/rand"
+	pb "github.com/nayotta/metathings/pkg/proto/deviced"
 )
 
 func generate_session() int32 {
@@ -13,4 +17,27 @@ func generate_session() int32 {
 
 func parse_bridge_id(device string, session int32) string {
 	return id_helper.NewNamedId(fmt.Sprintf("device.%v.session.%v", device, session))
+}
+
+func new_config_ack_message(sess int32) *pb.ConnectRequest {
+	return &pb.ConnectRequest{
+		SessionId: &wrappers.Int32Value{Value: sess},
+		Kind:      pb.ConnectMessageKind_CONNECT_MESSAGE_KIND_USER,
+		Union: &pb.ConnectRequest_StreamCall{
+			StreamCall: &pb.OpStreamCallValue{
+				Union: &pb.OpStreamCallValue_ConfigAck{
+					ConfigAck: &pb.OpStreamCallConfigAck{},
+				},
+			},
+		},
+	}
+}
+
+func new_config_ack_message_buffer(sess int32) []byte {
+	msg := new_config_ack_message(sess)
+	buf, err := proto.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return buf
 }
