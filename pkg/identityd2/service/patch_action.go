50cc13f00abef7660763831cfa9a2147a7936d8e
Peer Xu <pppeerxu@gmail.com>
2019-03-11 10:57:12 +0800

    :construction: add authorization and validation to `PatchAction` in a2

--- pkg/identityd2/service/patch_action.go
+++ pkg/identityd2/service/patch_action.go
@@ -6,9 +6,27 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	policy_helper "github.com/nayotta/metathings/pkg/common/policy"
 	storage "github.com/nayotta/metathings/pkg/identityd2/storage"
+	identityd_validator "github.com/nayotta/metathings/pkg/identityd2/validator"
 	pb "github.com/nayotta/metathings/pkg/proto/identityd2"
 )
+
+func (self *MetathingsIdentitydService) ValidatePatchAction(ctx context.Context, in interface{}) error {
+	return self.validator.Validate(
+		identityd_validator.Providers{
+			func() (policy_helper.Validator, action_getter) {
+				req := in.(*pb.PatchActionRequest)
+				return req, req
+			},
+		},
+		identityd_validator.Invokers{ensure_get_action_id},
+	)
+}
+
+func (self *MetathingsIdentitydService) AuthorizePatchAction(ctx context.Context, in interface{}) error {
+	return self.authorizer.Authorize(ctx, in.(*pb.PatchActionRequest).GetAction().GetId().GetValue(), "patch_action")
+}
 
 func (self *MetathingsIdentitydService) PatchAction(ctx context.Context, req *pb.PatchActionRequest) (*pb.PatchActionResponse, error) {
 	var err error
