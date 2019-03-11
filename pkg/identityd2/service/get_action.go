6be4fd8bdedf70c86265d5bf87fd412850d6f8fa
Peer Xu <pppeerxu@gmail.com>
2019-03-11 10:54:13 +0800

    :construction: add authorization and validation to `GetAction` in a2

--- pkg/identityd2/service/get_action.go
+++ pkg/identityd2/service/get_action.go
@@ -6,9 +6,27 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	policy_helper "github.com/nayotta/metathings/pkg/common/policy"
 	storage "github.com/nayotta/metathings/pkg/identityd2/storage"
+	identityd_validator "github.com/nayotta/metathings/pkg/identityd2/validator"
 	pb "github.com/nayotta/metathings/pkg/proto/identityd2"
 )
+
+func (self *MetathingsIdentitydService) ValidateGetAction(ctx context.Context, in interface{}) error {
+	return self.validator.Validate(
+		identityd_validator.Providers{
+			func() (policy_helper.Validator, action_getter) {
+				req := in.(*pb.GetActionRequest)
+				return req, req
+			},
+		},
+		identityd_validator.Invokers{ensure_get_action_id},
+	)
+}
+
+func (self *MetathingsIdentitydService) AuthorizeGetAction(ctx context.Context, in interface{}) error {
+	return self.authorizer.Authorize(ctx, in.(*pb.GetActionRequest).GetAction().GetId().GetValue(), "get_action")
+}
 
 func (self *MetathingsIdentitydService) GetAction(ctx context.Context, req *pb.GetActionRequest) (*pb.GetActionResponse, error) {
 	var err error
