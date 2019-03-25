85e4e3af93018b8325a1afce61166dee07ddfb95
Peer Xu <pppeerxu@gmail.com>
2019-03-25 11:06:12 +0800

    :construction: add `GetObjectContent` to deviced service

--- pkg/deviced/service/get_object_content.go
+++ pkg/deviced/service/get_object_content.go
@@ -3,9 +3,66 @@ package metathings_deviced_service
 import (
 	"context"
 
+	"github.com/golang/protobuf/ptypes/wrappers"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	policy_helper "github.com/nayotta/metathings/pkg/common/policy"
+	identityd_validator "github.com/nayotta/metathings/pkg/identityd2/validator"
 	pb "github.com/nayotta/metathings/pkg/proto/deviced"
 )
 
-func (self *MetathingsDevicedService) GetObjectContent(context.Context, *pb.GetObjectContentRequest) (*pb.GetObjectContentResponse, error) {
-	panic("unimplemented")
+func (self *MetathingsDevicedService) ValidateGetObjectContent(ctx context.Context, in interface{}) error {
+	return self.validator.Validate(
+		identityd_validator.Providers{
+			func() (policy_helper.Validator, object_getter) {
+				req := in.(*pb.GetObjectContentRequest)
+				return req, req
+			},
+		},
+		identityd_validator.Invokers{
+			ensure_get_object_name,
+			ensure_get_object_device_id,
+		},
+	)
+}
+
+func (self *MetathingsDevicedService) AuthorizeGetObjectContent(ctx context.Context, in interface{}) error {
+	return self.authorizer.Authorize(ctx, in.(*pb.GetObjectContentRequest).GetObject().GetDevice().GetId().GetValue(), "get_object_content")
+}
+
+func (self *MetathingsDevicedService) GetObjectContent(ctx context.Context, req *pb.GetObjectContentRequest) (*pb.GetObjectContentResponse, error) {
+	obj := req.GetObject()
+	obj_s := parse_object(obj)
+
+	dev_s, err := self.storage.GetDevice(obj_s.Device)
+	if err != nil {
+		self.logger.WithError(err).Errorf("failed to get device in storage")
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+
+	ch, err := self.simple_storage.GetObjectContent(dev_s, obj_s)
+	if err != nil {
+		self.logger.WithError(err).Errorf("failed to get object content in simple storage")
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+
+	var contents []byte
+	for {
+		buf, ok := <-ch
+		if !ok {
+			break
+		}
+		contents = append(contents, buf...)
+	}
+
+	res := &pb.GetObjectContentResponse{Content: &wrappers.BytesValue{Value: contents}}
+
+	self.logger.WithFields(log.Fields{
+		"device": obj_s.Device,
+		"object": obj_s.FullName(),
+	}).Debugf("get object content")
+
+	return res, nil
 }
