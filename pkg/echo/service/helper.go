517afde069fa359c3dfcaf1df817a683b37c1034
Peer Xu <pppeerxu@gmail.com>
2019-05-07 10:25:55 +0800

    :fire: remove files

--- pkg/echo/service/helper.go
+++
@@ -1,9 +0,0 @@
-package metathings_echo_service
-
-import (
-	grpc_helper "github.com/nayotta/metathings/pkg/common/grpc"
-)
-
-func (srv *metathingsEchoService) handleGRPCError(err error, format string, args ...interface{}) error {
-	return grpc_helper.HandleGRPCError(srv.logger, err, format, args)
-}
