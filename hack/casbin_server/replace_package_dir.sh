3ed2b6fcaf4db9a076ce18ad44f77b7fac39921e
PeerHui <pppeerxu@gmail.com>
2024-11-01 14:08:34 +0800

    replace_package_dir.sh works with macOS

--- hack/casbin_server/replace_package_dir.sh
+++ hack/casbin_server/replace_package_dir.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-sed -i 's|go_package = "./;proto";|go_package = "github.com/casbin/casbin-server/proto";|g' vendor/github.com/casbin/casbin-server/proto/casbin.proto
+SED="sed"
+if [ "x$(uname -s)" == "xDarwin" ]; then
+    SED="gsed"
+    which $SED > /dev/null
+    if [ $? == 1 ]; then
+        echo "require gsed for macOS"
+        exit 1
+    fi
+fi
+
+$SED -i 's|go_package = "./;proto";|go_package = "github.com/casbin/casbin-server/proto";|g' vendor/github.com/casbin/casbin-server/proto/casbin.proto
